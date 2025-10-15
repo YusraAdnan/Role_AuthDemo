532f9707738fdb315c250d03d2e5d4ad1780d7e6
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-10-15 21:29:26 +0200

    Show Role Based Navigation Items based on sessions

--- Role_AuthDemo/Controllers/EmployeeController.cs
+++ Role_AuthDemo/Controllers/EmployeeController.cs
@@ -1,17 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Role_AuthDemo.Models;
 using System.Data;
 
 namespace Role_AuthDemo.Controllers
 {
     public class EmployeeController : Controller
     {
+        //Just say now you have to add function to allow Employee to view their own leaves that they had applied for
+        List<LeaveRequest> leave = new List<LeaveRequest>() {
+         
+        //For ease and demo purpose I haven't added this to the DB you can do this part
+        new LeaveRequest {
+                Id = Guid.NewGuid(),
+                UserId = 2,
+                StartDate = new DateTime(2025, 10, 1),
+                EndDate = new DateTime(2025, 10, 5),
+                Reason = "Medical Leave",
+                Status = "Approved"
+            },
+        new LeaveRequest {
+                Id = Guid.NewGuid(),
+                UserId = 2,
+                StartDate = new DateTime(2025, 10, 1),
+                EndDate = new DateTime(2025, 10, 5),
+                Reason = "Wedding Leave",
+                Status = "Approved"
+            },
+            new LeaveRequest
+            {
+                Id = Guid.NewGuid(),
+                UserId = 3,
+                StartDate = new DateTime(2025, 10, 10),
+                EndDate = new DateTime(2025, 10, 15),
+                Reason = "Vacation Trip",
+                Status = "Pending"
+            }
+        };
         public IActionResult EmployeeDashboard()
         {
             //Get the current logged in persons role and email (if you want to show the users email as well in the view)
             var role = HttpContext.Session.GetString("UserRole");
             var email = HttpContext.Session.GetString("UserEmail");
 
-            /*The employee dashboard should ONLY be visible if the logged in persons session role is Employee
+            /* The employee dashboard should ONLY be visible if the logged in persons session role is Employee
              If it is not then redirect them to the login page */
             if (role != "Employee" || role == null)
             {
@@ -23,6 +55,18 @@ namespace Role_AuthDemo.Controllers
             }
             ViewBag.Email = email;
             return View();
+        }
+
+        public IActionResult ViewLeaves()
+        { 
+            //Get the current logged in sessions userId
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+
+            //Filter the requests to view leaves based on that ID ONLY, ensuring user based info to be shown
+            var requests = leave.Where(l => l.UserId == userId).ToList();
+
+            return View(requests);
         }
 
     }
---
+++ Role_AuthDemo/Models/LeaveRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Role_AuthDemo.Models
+{
+    public class LeaveRequest
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; }       // Link to Employee
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+        public string? DocumentPath { get; set; }  // Uploaded file
+        public string Status { get; set; } = "Pending"; // Pending / Approved / Rejected
+
+        public int TotalDays
+        {
+            get
+            {
+                return (EndDate - StartDate).Days + 1;
+            }
+        }
+
+
+    }
+}
